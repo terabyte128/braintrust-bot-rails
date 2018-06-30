d6de2e6baf9fa6e2dc2a26267cb8d04c5bbcf660
Sam Wolfson <wolfson@cs.uw.edu>
2018-06-30 00:51:20 -0700

    capitalize, not humanize

--- lib/helpers/application_helpers.rb
+++ lib/helpers/application_helpers.rb
@@ -1,7 +1,7 @@
 module ApplicationHelpers
   # Build an HTML-formatted quote to display to the user
   def format_quote(content, author, context, date)
-    quote = "\"<i>#{content.humanize}</i>\"\n<b> - #{author.titleize} #{date}</b>"
+    quote = "\"<i>#{content.capitalize}</i>\"\n<b> - #{author.titleize} #{date}</b>"
     if context.present?
       quote << " (#{context})"
     end
