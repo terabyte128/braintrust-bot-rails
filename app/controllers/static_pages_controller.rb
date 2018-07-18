835a56cb07afb66920474df816a25f515a6c1f49
Sam Wolfson <wolfson@cs.uw.edu>
2018-07-17 21:04:03 -0700

    Add luck statistics for all chat members

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,13 @@ class StaticPagesController < ApplicationController
       if params[:member]
         @member = @chat.members.find(params[:member])
       end
+
+      @combined_luck = @chat.members.map do |m|
+        {
+            name: m.display_name,
+            data: m.luck_histories.map { |l| [ l.created_at, l.luck ] }
+        }
+      end
     else
       @chat = nil
     end
