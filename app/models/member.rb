fd981fab13fecec46c60068cb537bd8f7a892b36
Sam Wolfson <wolfson@cs.uw.edu>
2019-03-12 12:28:11 -0700

    remove chat-weighted luck for now

--- app/models/member.rb
+++ app/models/member.rb
@@ -53,20 +53,7 @@ class Member < ApplicationRecord
     elsif rand < 0.4
       new_luck = sample_random
     else
-      avg_luck = (chats.map{ |c| c.average_luck }).sum / chats.count
-
-      if rand < 0.32274
-        # weight the luck slightly by the average luck of the chat
-        diff = (avg_luck - luck).abs ** (2.0 / 3)
-
-        if rand < 0.5
-          diff *= -1
-        end
-
-        new_luck = sample_z_estimate(diff)
-      else
-        new_luck = sample_z_estimate
-      end
+      new_luck = sample_z_estimate
     end
 
     update_luck new_luck
